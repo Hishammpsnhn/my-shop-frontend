9304370818daa38a1399d86ab95eee079107bbdf
hisham <hishammpsn@gmail.com>
2023-03-09 21:21:12 +0530

    added login screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes, } from "react-router-dom";
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 import ProductScreen from './screens/ProductScreen';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route path="/login" element={<LoginScreen />} />
         <Route path="/product" element={<ProductScreen />} />
         <Route path="/search/:keyword" element={<HomeScreen />} />
         <Route path="/" element={<HomeScreen />} />
---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+function LoginScreen() {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [incorrectPassword, setIncorrectPassword] = useState<boolean>(false)
+
+    return (
+        <div className="mt-10">
+            <div className=" m-auto max-w-xs">
+                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" >
+                            Email
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" >
+                            Password
+                        </label>
+                        <input className={`shadow appearance-none border ${incorrectPassword && " border-red-500"} rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+                            id="password" type="password"
+                            placeholder="******************"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        {incorrectPassword &&
+                            <p className="text-red-500 text-xs italic">Please choose a password.</p>}
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                            Sign In
+                        </button>
+                        <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                            Forgot Password?
+                        </a>
+                    </div>
+                </form>
+                <p className="text-center text-gray-500 text-xs">
+                    &copy;2020 Acme Corp. All rights reserved.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default LoginScreen;
