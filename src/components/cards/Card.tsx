5f937f4078a0491142119a8cc36c09e95e5b307b
hisham <hishammpsn@gmail.com>
2023-07-29 17:20:39 +0530

    image error fix

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Rating from './Rating';
+import API_BASE_URL from '../../config/config';
 
 interface props {
   name: string;
@@ -13,18 +14,18 @@ interface props {
 
 function Card({ name, image, rating, price, reviewLength, id }: props) {
   return (
-    <div className=" rounded  overflow-hidden p-2 border">
-      <Link to={`/product/${id}`}>
-        <img className="w-full " src={image} alt="Sunset in the mountains" />
-      </Link>
-      <div className="px-4 pt-2 pb-5">
-        <p className="text-gray-800 font-medium text-base capitalize ">
-          {name}
-        </p>
-        <Rating rating={rating} reviewCount={reviewLength} color={'orange'} />
-        <div className="font-bold text-xl mb-2">$ {price}</div>
+    <Link to={`/product/${id}`}>
+      <div className=" rounded  overflow-hidden p-2 border">
+        <img className="w-full " src={`${API_BASE_URL}${image}`} alt={name} />
+        <div className="px-4 pt-2 pb-5">
+          <p className="text-gray-800 font-medium text-base capitalize ">
+            {name}
+          </p>
+          <Rating rating={rating} reviewCount={reviewLength} color={'orange'} />
+          <div className="font-bold text-xl mb-2">$ {price}</div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
