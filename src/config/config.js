8fb75eafef0f8bcb57a25081ac3af0adc0caf923
hisham <hishammpsn@gmail.com>
2023-07-29 10:10:00 +0530

    add server api

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 // config.js
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://13.53.77.212:4000';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://www.hishammpsn.tk';
 export default API_BASE_URL;
